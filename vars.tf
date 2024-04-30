4e5a17b49e8eae2bf5892e087393c219d8de12b1
Eugenio Pastoral <pastoraleugenio@gmail.com>
2024-05-01 02:54:11 +0800

    Add Terraform variables

---
+++ vars.tf
@@ -0,0 +1,40 @@
+# SSH key variable used in terraform.tfvars
+variable "ssh_key" {
+}
+
+# Token ID variable used in terraform.tfvars
+variable "token_id" {
+}
+
+# Token secret variable used in terraform.tfvars
+variable "token_secret" {
+}
+
+# Proxmox API URL
+variable "api_url" {
+}
+
+# Proxmox Hostname
+variable "proxmox_host" {
+  default = "homelab"
+}
+
+# Proxmox Storage
+variable "storage_backend" {
+  default = "local-lvm"
+}
+
+# Proxmox VM Template
+variable "template_name" {
+  default = "ubuntu-2404-template"
+}
+
+# Network Device
+variable "nic_name" {
+  default = "vmbr0"
+}
+
+# VLAN
+variable "vlan_num" {
+  default = "10"
+}
