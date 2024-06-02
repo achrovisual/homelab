294ce7dba2f7dcc86396be7b541234196e7c81c7
Eugenio Pastoral <pastoraleugenio@gmail.com>
2024-06-02 15:15:10 +0800

    Update k3s module to deploy small and medium instances from different Proxmox nodes

--- terraform/modules/k3s/main.tf
+++ terraform/modules/k3s/main.tf
@@ -1,22 +1,20 @@
-resource "proxmox_vm_qemu" "k3s" {
-  name = "k3s-${format("%02d", count.index + 1)}"
-  vmid = var.k3s_vmid + count.index + 1
-  count = 3
-  target_node = var.proxmox_host
-  clone = var.template_name
+resource "proxmox_vm_qemu" "k3s_admin" {
+  name        = "k3s-admin"
+  vmid        = var.hl1_k3s_offset + 1
+  target_node = var.proxmox_host_primary
+  clone       = var.template_name
   full_clone  = "true"
 
-  agent = 1
-  os_type = "cloud-init"
+  agent     = 1
+  os_type   = "cloud-init"
   ipconfig0 = "ip=dhcp"
-  sshkeys = var.ssh_key
+  sshkeys   = var.ssh_key
 
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = 4096
-  balloon = 2048
-  scsihw = "virtio-scsi-pci"
+  cores    = 2
+  sockets  = 1
+  cpu      = "host"
+  memory   = 1024
+  scsihw   = "virtio-scsi-pci"
   bootdisk = "scsi0"
 
   disks {
@@ -30,8 +28,8 @@ resource "proxmox_vm_qemu" "k3s" {
     scsi {
       scsi0 {
         disk {
-          size = "3584M"
-          storage = var.storage_backend
+          size       = "3584M"
+          storage    = var.storage_backend
           emulatessd = true
         }
       }
@@ -39,8 +37,114 @@ resource "proxmox_vm_qemu" "k3s" {
   }
 
   network {
-    model = "virtio"
-    bridge = var.nic_name
+    model    = "virtio"
+    bridge   = var.nic_name
+    firewall = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "k3s_nodes_small" {
+  name        = "k3s-${format("%02d", count.index + 1)}"
+  vmid        = var.hl1_k3s_offset + 1 + count.index + 1
+  count       = var.hl1_k3s_count
+  target_node = var.proxmox_host_primary
+  clone       = var.template_name
+  full_clone  = "true"
+
+  agent     = 1
+  os_type   = "cloud-init"
+  ipconfig0 = "ip=dhcp"
+  sshkeys   = var.ssh_key
+
+  cores    = 2
+  sockets  = 1
+  cpu      = "host"
+  memory   = 2048
+  balloon  = 1024
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.storage_backend
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size       = "3584M"
+          storage    = var.storage_backend
+          emulatessd = true
+        }
+      }
+    }
+  }
+
+  network {
+    model    = "virtio"
+    bridge   = var.nic_name
+    firewall = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "k3s_nodes_medium" {
+  name        = "k3s-${format("%02d", count.index + 1 + var.hl1_k3s_count)}"
+  vmid        = var.hl2_k3s_offset + count.index + 1
+  count       = 2
+  target_node = var.proxmox_host_secondary
+  clone       = var.template_name
+  full_clone  = "true"
+
+  agent     = 1
+  os_type   = "cloud-init"
+  ipconfig0 = "ip=dhcp"
+  sshkeys   = var.ssh_key
+
+  cores    = 2
+  sockets  = 1
+  cpu      = "host"
+  memory   = 4096
+  balloon  = 2048
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.storage_backend
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size       = "3584M"
+          storage    = var.storage_backend
+          emulatessd = true
+        }
+      }
+    }
+  }
+
+  network {
+    model    = "virtio"
+    bridge   = var.nic_name
     firewall = true
   }
 
