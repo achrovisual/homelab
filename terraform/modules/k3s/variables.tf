09996f26524efa0c5437c2a4dd185b9caa9741a6
Eugenio Pastoral <pastoraleugenio@gmail.com>
2024-06-02 21:44:59 +0800

    Remove traces of second Proxmox node

--- terraform/modules/k3s/variables.tf
+++ terraform/modules/k3s/variables.tf
@@ -19,12 +19,6 @@ variable "proxmox_host_primary" {
   default = "hl-pve-01"
 }
 
-# Proxmox Hostname
-variable "proxmox_host_secondary" {
-  default = "hl-pve-02"
-}
-
-
 # Proxmox Storage
 variable "storage_backend" {
   default = "local-lvm"
@@ -50,17 +44,7 @@ variable "hl1_k3s_offset" {
   default = 100
 }
 
-# Proxmox Node 2 - Kubernetes VMID Offset
-variable "hl2_k3s_offset" {
-  default = 200
-}
-
 # Proxmox Node 1 - Kubernetes Node Count
 variable "hl1_k3s_count" {
   default = 3
-}
-
-# Proxmox Node 2 - Kubernetes Node Count
-variable "hl2_k3s_count" {
-  default = 2
 }
